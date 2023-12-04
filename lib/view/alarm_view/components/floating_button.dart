b505d6a5de805141f4c48bf15ba7d422fdd9d07d
EricssonXD <114271127+EricssonXD@users.noreply.github.com>
2023-12-04 23:32:03 +0800

    floating neumorphic button

--- lib/view/alarm_view/components/floating_button.dart
+++ lib/view/alarm_view/components/floating_button.dart
@@ -1,25 +1,27 @@
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:snooze_slayer/view/alarm_view/alarm_edit_view.dart';
-import 'package:flutter/material.dart';
-
-import '../../common_widget/soft_button.dart';
 
 class FloatingButton extends StatelessWidget {
   const FloatingButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.of(context, rootNavigator: true).push(
-            MaterialPageRoute(builder: (context) => const AlarmEditView()));
-      },
-      child: CircularSoftButton(
-        radius: 30,
-        icon: const Icon(
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15.0),
+      child: NeumorphicFloatingActionButton(
+        style: const NeumorphicStyle(
+          boxShape: NeumorphicBoxShape.circle(),
+          lightSource: LightSource.topLeft,
+        ),
+        child: const Icon(
           Icons.add,
           color: Colors.pinkAccent,
           size: 30,
         ),
+        onPressed: () {
+          Navigator.of(context, rootNavigator: true).push(
+              MaterialPageRoute(builder: (context) => const AlarmEditView()));
+        },
       ),
     );
   }
